83be4f167ecbe394454f1b647a624d8773ed436e
Ali <alihossen01646@gmail.com>
2026-08-18 12:21:59 +0600

    Project All most Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,20 +35,110 @@ const scrollHeader = () =>{
    this.scrollY >= 50 ? header.classList.add('shadow-header') 
                       : header.classList.remove('shadow-header')
 }
-window.addEventListener('scroll', shadowHeader)
+window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SWIPER PRICES ===============*/
 
-
+const pricesSwiper = new Swiper('.prices__swiper', {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: '.prices__swiper .swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 350
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						     : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+// Link the ID of each section (section id="home") to each link (a href="#home") 
+// and activate the link with the class .active-link
+const scrollActive = () => {
+   // We get the position by scrolling down
+   const scrollY = window.scrollY
+
+   sections.forEach(section => {
+      const id = section.id, // id of each section
+            top = section.offsetTop - 50, // Distance from the top edge
+            height = section.offsetHeight, // Element height
+            link = document.querySelector('.nav__menu a[href*=' + id + ']') // id nav link
+
+      if(!link) return
+
+      link.classList.toggle('active-link', scrollY > top && scrollY <= top + height)
+   })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-fill'
 
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-fill' : 'ri-sun-fill'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-fill' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2000,
 
+})
+
+sr.reveal('.home__content', {origin: 'bottom'})
+sr.reveal('.home__info', {origin: 'bottom', delay: 800})
+sr.reveal('.home__data', {delay: 1400})
+sr.reveal('.home__button', {origin: 'left', delay: 1800})
+
+sr.reveal('.delivery__data', {origin: 'right'})
+sr.reveal('.delivery__content', {origin: 'left', delay: 600})
+sr.reveal('.delivery__img', {delay: 1200})
+
+sr.reveal('.about__data , .contact__map',  {origin: 'left'})
+sr.reveal('.about__img , .contact__data', {origin: 'right'})
+
+sr.reveal('.prices__box')
+sr.reveal('.prices__swiper', {origin: 'bottom', delay: 600})
+
+sr.reveal('.gallery__image', {interval: 100})
+
+sr.reveal('.footer__container')
